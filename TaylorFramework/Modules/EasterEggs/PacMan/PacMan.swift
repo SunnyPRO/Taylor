1e10a678c46997ce812275cf911150ba7a2d1436
SunnyPRO <sunnypro@inbox.ru>
2016-11-01 16:39:20 +0200

    Add temporary fix for EasterEggs
    Also added small fixes to SourceKitten to satisfy Scissors module also some other small changes

--- TaylorFramework/Modules/EasterEggs/PacMan/PacMan.swift
+++ TaylorFramework/Modules/EasterEggs/PacMan/PacMan.swift
@@ -33,16 +33,19 @@ final class Pacman {
     If no paths were given, no game instance will run.
     */
     func start() {
+        //TODO: Review/redo pacman execution
+        removeMap()
         guard !paths.isEmpty && createMap() else {
             print("An unexpected error occured when launching Pacman.")
             return
         }
+        let task = Process()
         let path = getGamePath()
         
-        let process = Process()
-        
-        process.launchPath = path
-        process.arguments = ["python", path, "/pacman.py"]
+        task.launchPath = "/usr/bin/open"
+        task.arguments = ["-a", "Terminal.app", path.stringByAppendingPathComponent("pacman.py")]
+        task.launch()
+        sleep(4)
         removeMap()
     }
     
